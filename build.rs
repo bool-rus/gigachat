3f14a6e1e35bf9ae29ff6e6e7244230c1e705271
Bool <bool.rus@gmail.com>
2025-04-05 14:00:34 +0300

    added serde to prost generated types

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("running prost codegen");
     tonic_build::configure().build_server(false)
-        .protoc_arg("--experimental_allow_proto3_optional")
-        //.client_attribute(".", "#[derive(derive_more::From, derive_more::Into)]")
-        //.type_attribute("Quotation", "#[derive(Eq, Ord, PartialOrd)]")
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile_protos(
             &[
                 "proto/gigachatv1.proto",
